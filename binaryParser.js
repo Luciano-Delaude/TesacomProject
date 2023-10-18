92d2e4a17b66ac784bfb6ba9528ee3088ced0292
Luciano-Delaude <lucho.M.D_04@hotmail.com>
2023-10-18 01:14:29 -0300

    Working decoder withouth signed int and partial buffer

--- binaryParser.js
+++ binaryParser.js
@@ -6,23 +6,71 @@ class BinaryParser {
    * @param {Array} format - Formato de serialización
    * @returns {Object} Objeto deserializado con campos tag = valor
    */
-  decode(buffer, format) {
-      let offset = 8;
-      let decodedObject = {};
-      let values = 0;
-      buffer.forEach(element => {
-        values =(values << offset) | element;
-        console.log(values.toString(2));
-      });
-      let restOfData = buffer.length * 8;
-      format.forEach(field => {
-        decodedObject[field.tag] = values >> (restOfData - field.len);
-        values = values & ((1 << (restOfData - field.len)) - 1)
-        restOfData = restOfData - field.len;
-      });
+   decode(buffer, format) {
+    let first32Bits = 0;
+    // Extract the first 4 bytes (32 bits) from the buffer
+    if (buffer.length > 4) {
+      first32Bits = buffer.readUIntBE(0,4);
+    } else {
+      first32Bits = buffer.readUIntBE(0,buffer.length);
+    }
 
-      return decodedObject;
-  }
+    // Extract the next bytes from the buffer
+    const remainingBytes = buffer.slice(buffer.length - 4);
+
+    let offset = 0;
+    let values = first32Bits;
+    let decodedObject = {};
+
+    format.forEach(field => {
+        if (field.type == "float") {
+            field.len = 32;
+        }
+        let mask = (1 << field.len) - 1;
+        let fieldValue;
+
+        if (offset < 32) {
+            // If the field is within the first 32 bits, extract the value from the first32Bits
+            fieldValue = values >>> (32 - (offset + field.len)) & mask;
+        } else {
+            // If the field extends beyond the first 32 bits, extract the value from the remaining bytes
+            const byteOffset = Math.ceil(offset / 8) - 4;
+            console.log(offset);
+            const bitOffset = offset % 8;
+            if (field.type == 'float') {
+              fieldValue = remainingBytes.readUIntBE(byteOffset, Math.ceil(field.len / 8));
+            } else {
+              fieldValue = remainingBytes.readUIntBE(byteOffset, Math.ceil(field.len / 8)) >>> bitOffset & mask;
+            }
+            console.log(fieldValue);
+        }
+        offset += field.len;
+
+        switch (field.type) {
+            case 'int':
+                // If the value is negative, convert it to a signed integer
+                if ((fieldValue & (1 << (field.len - 1))) !== 0) {
+                    fieldValue = fieldValue - (1 << field.len);
+                }
+                decodedObject[field.tag] = fieldValue;
+                break;
+            case 'uint':
+                decodedObject[field.tag] = fieldValue;
+                break;
+            case 'float':
+                // Create a buffer with the extracted bits and convert it to a float
+                const floatBuffer = Buffer.alloc(4);
+                floatBuffer.writeUint32BE(fieldValue, 0, 4);
+                decodedObject[field.tag] = floatBuffer.readFloatBE(0);
+                break;
+            default:
+                break;
+        }
+    });
+
+    return decodedObject;
+}
+
 
   /**
    * Codifica un objeto según el formato especificado.
@@ -38,23 +86,41 @@ class BinaryParser {
 
     format.forEach(field => {
         const value = _object[field.tag];
-        if (field.hasOwnProperty('len')) {
-          partialData = (partialData << field.len) | value;
-          partialBits += field.len; 
-        } else {
-          partialData = (partialData << 32) | value;
-          partialBits += 32; 
-        }
-        while (partialBits >= 32) {
-            // If we have accumulated 32 or more bits, write it to a new buffer
-            const buffer = Buffer.alloc(4);
-            buffer.writeUInt32BE(partialData >>> (partialBits - 32), 0);
+        const buffer = Buffer.alloc(4);
+
+        switch (field.type) {
+          case 'int':
+            partialData = (partialData << field.len) | value;
+            partialBits += field.len; 
+            while (partialBits >= 32) {
+              buffer.writeIntBE(partialData >>> (partialBits - 32), 0, 4);
+              // If we have accumulated 32 or more bits, write it to a new buffer
+              buffers.push(buffer);
+              // Remove the written bits from partialData
+              partialData = partialData & ((1 << (partialBits - 32)) - 1);
+              partialBits -= 32;
+            }
+
+            break;
+          case 'uint':
+            partialData = (partialData << field.len) | value;
+            partialBits += field.len; 
+            while (partialBits >= 32) {
+              buffer.writeUInt32BE(partialData >>> (partialBits - 32), 0);
+              // If we have accumulated 32 or more bits, write it to a new buffer
+              buffers.push(buffer);
+              // Remove the written bits from partialData
+              partialData = partialData & ((1 << (partialBits - 32)) - 1);
+              partialBits -= 32;
+            }
+            break;
+          case 'float':
+            buffer.writeFloatBE(value, 0, 4);
             buffers.push(buffer);
-            
-            // Remove the written bits from partialData
-            partialData = partialData & ((1 << (partialBits - 32)) - 1);
-            partialBits -= 32;
-        }
+            break;
+          default:
+            break;
+        }        
     });
     // Write any remaining data (less than 32 bits) to the buffer
     if (partialBits > 0) {
@@ -72,10 +138,11 @@ class BinaryParser {
 const format1 = [
   { tag: "PTemp", type: "int", len: 12 },
   { tag: "BattVolt.value", type: "int", len: 12 },
-  { tag: "WaterLevel", type: "int", len: 8 }
+  { tag: "WaterLevel", type: "int", len: 8 },
+  {tag: "Pressure", type: "float"}
 ];
 
-const data = { PTemp: 268, "BattVolt.value": 224, WaterLevel: 115 };
+const data = { PTemp: 268, "BattVolt.value":-4, WaterLevel: 115, Pressure: 52.4 };
 
 // const format1 = [{ tag: "var0.value", type: "uint", len: 2 },
 // { tag: "var1.value", type: "uint", len: 2 },
